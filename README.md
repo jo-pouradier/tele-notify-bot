80e759cd926623111848d46af02ee7872b3cada3
jo-pouradier <jocharbo43@gmail.com>
2024-05-25 20:32:29 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# My telegram bot to get homelab notification
+
+for metrics use [gopsutil](https://github.com/shirou/gopsutil).
+
+
---
+++ metrics/tests.go
@@ -0,0 +1,30 @@
+package metrics
+
+import (
+	"time"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
+)
+
+func GetCPU1() (float64, error) {
+	c, err := cpu.Percent(1*time.Second, false)
+	if err != nil {
+		return 0, err
+	}
+	if len(c) > 0 {
+		return c[0], nil
+	}
+	return 0, nil
+
+}
+
+func GetMEM1() (float64, error) {
+	m, err := mem.VirtualMemory()
+	if err != nil {
+		return 0, err
+	}
+
+	return m.UsedPercent, nil
+
+}
